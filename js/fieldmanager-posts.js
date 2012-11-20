f02d7a0b18fbfe5b14cfe1eb4046f35e66bf1a85
bcampeau <brad@alleyinteractive.com>
2012-11-20 10:43:00 -0500

    Fixed issue with plugin dependency class conflict. Finished algorithm for related post search.

--- js/fieldmanager-posts.js
+++ js/fieldmanager-posts.js
@@ -8,9 +8,31 @@ $( document ).ready( function () {
 		if ( $("#post_ID").val() != "" ) {
 			// Store the element we are working with
 			fm_posts_element = $(this).data('relatedElement');
+			var fm_taxonomy_terms = {};
+			
+			// Get all the currently selected taxonomy terms in single taxonomy fields for use in finding related content
+			// This must be done using Javascript since terms may have changed in the interface before being committed to the database
+			$(".fm-options").each(function () {
+				// Only use option fields where a taxonomy is specified
+				//console.log("taxonomy:" + $(this).data("taxonomy"));
+				var fm_taxonomy = JSON.parse($(this).data("taxonomy"));
+				if( fm_taxonomy != undefined ) {
+					var fm_terms = new Array();
+					$("option:selected", this).each(function () {
+						//console.log("value:" + $(this).val());
+						fm_terms.push( $(this).val() );
+					});
+					if ( fm_terms.length > 0 ) fm_taxonomy_terms[$(this).data( "taxonomy" )] = fm_terms;
+				}
+			});
 		
+			//console.log(fm_taxonomy_terms);
+			var fm_taxonomy_terms_json = JSON.stringify(fm_taxonomy_terms);
+			//console.log(fm_taxonomy_terms_json);
+
 			// Query for matching terms
-			$.post( ajaxurl, { action: 'fm_posts_find_related', post_id: $("#post_ID").val(), post_types: $(this).data("postTypes"), fm_posts_find_related_nonce: fm_posts.nonce }, function ( result ) {
+			$.post( ajaxurl, { action: 'fm_posts_find_related', post_id: $("#post_ID").val(), post_terms: fm_taxonomy_terms_json, post_types: $(this).data("postTypes"), fm_posts_find_related_nonce: fm_posts.nonce }, function ( result ) {
+				console.log( result );
 				resultObj = JSON.parse( result );
 				// Check if there were results
 				if( !$.isEmptyObject( resultObj ) ) {
@@ -22,7 +44,7 @@ $( document ).ready( function () {
 					});
 					
 					// Also trigger a jQuery event other custom theme scripts can bind to if needed
-					$( "#" + fm_terms_element ).trigger( 'fm_posts_related' );
+					$( "#" + fm_posts_element ).trigger( 'fm_posts_related' );
 
 				}
 				
--- php/class-fieldmanager-posts.php
+++ php/class-fieldmanager-posts.php
@@ -5,8 +5,11 @@ class Fieldmanager_Posts {
 	/** @type string Fieldmanager field name to augment with related post search */ 
 	public $field_name;
 	
-	/** @type boolean Indicates if we should automatically find related posts */ 
-	public $automatic = false;
+	/** @type boolean Specify a minimum length under which content would not searched for terms */ 
+	public $minimum_content_length;
+	
+	/** @type boolean Specify the maximum links to return */ 
+	public $maximum_links = 5;
 	
 	/** @type string Label for related posts search button */ 
 	public $button_label = "Find Related Posts";
@@ -48,16 +51,19 @@ class Fieldmanager_Posts {
 	public function ajax_find_related_posts() {
 		// Check the nonce before we do anything
 		check_ajax_referer( 'fm_posts_find_related_nonce', 'fm_posts_find_related_nonce' );
-		
+		error_log("1\n");
 		// Create an array to hold the results.
 		$result = array();
-				
+		error_log("2\n");
 		// Pass the post title and content to term extraction if one if them is not empty. 
 		// Otherwise return the empty array.
-		if( trim( $_POST['post_id'] ) ) {
-			$result  = $this->related_posts( $_POST['post_id'] ) );
+		error_log("3\n");
+		if( !empty( $_POST['post_id'] ) && !empty( $_POST['post_terms'] ) ) {
+			error_log("4\n");
+			$result = $this->related_posts( $_POST['post_id'], $_POST['post_terms'] );
+			error_log("5\n");
 		}
-
+		error_log("6\n");
 		echo json_encode( $result );
 		
 		die();
@@ -69,10 +75,71 @@ class Fieldmanager_Posts {
 	 * @params string $post_type
 	 * @return void
 	 */
-	public function related_posts( $post_id ) {
-				
+	public function related_posts( $post_id, $post_terms ) {
+		error_log("a\n");		
+		// Holds the return values
 		$post_matches = array();
+		error_log("b\n");
+		// Convert terms to an object
+		$terms = json_decode( stripslashes( $post_terms ) );
+		error_log("c\n");
+		$related_post_results = array();
+		error_log("d\n");
+		// Query each taxonomy and term individually because we need to know how many matches occurred for each to rank the final list
+		error_log( print_r( get_object_vars( $terms ), true ) );
+		foreach( get_object_vars( $terms ) as $taxonomy => $terms ) {
+			error_log("e\n");
+			$tax_name = json_decode($taxonomy);
+			error_log("f\n");
+			foreach( $terms as $term_id ) {
+				$args = array(
+					'post__not_in' => array( $post_id ), // exclude the post we are trying to find related content for
+					'tax_query' => array(
+						array( 
+							'taxonomy' => $tax_name,
+							'field' => 'id',
+							'terms' => $term_id
+						)
+					),
+					'orderby' => 'date', // always order by newest first
+					'order' => 'DESC',
+					'posts_per_page' => 50
+				);
+				$query = new WP_Query( $args );
+				
+				error_log( $taxonomy . ":" . $term_id );
+				error_log( print_r( $query->request, true ) );
+								
+				while( $query->have_posts() ) {
+					 $query->next_post();
+					 // See if this post was found previously.
+					 if( array_key_exists( $query->post->ID, $related_post_results ) ) {
+					 	// Post already matched. Just increment the match count.
+					 	$related_post_results[$query->post->ID]['count']++;
+					 } else {
+					 	// First match. Add to the array.
+						$post_type = get_post_type_object( get_post_type( $query->post->ID ) );
+					 	$related_post_results[$query->post->ID] = array(
+					 		'count' => 1,
+					 		'id' => $query->post->ID,
+					 		'post_type' => $post_type->labels->singular_name,
+					 		'post_date' => get_the_date( $query->post->ID ),
+					 		'post_title' => get_the_title( $query->post->ID ), 
+					 		'permalink' => get_permalink( $query->post->ID )
+					 	);
+					 }
+				}
+			}
+		}
 		
+		// Perform a final sort on the data to favor posts with the most matches
+		uasort( $related_post_results, function( $a, $b ) {
+			 return ( $a['count'] > $b['count'] ) ? -1 : 1;
+		} );
+		
+		// Return the slice of the array up to the max post limit
+		error_log( print_r( $related_post_results, true ) );
+		return array_slice( $related_post_results, 0, $this->maximum_links );			
 	}
 	
 	/**
@@ -84,7 +151,7 @@ class Fieldmanager_Posts {
 		// This functionality is only enabled for Fieldmanager_Post.
 		// If so, add the suggest button. Otherwise return the element unmodified.
 		if ( $field->name == $this->field_name 
-			&& get_class( $field ) == "Fieldmanager_Post"
+			&& ( get_class( $field ) == "Fieldmanager_Post" )
 			&& isset( $field->post_types )
 			&& !empty( $field->post_types ) ) $value .= $this->find_related( $field->get_element_id(), $field->post_types );
 		
